2c4b7d93826bb61783eee810671dffc9242c0d4e
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-11 01:52:25 -0400

    Implement user deletion route and export userRouter module

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,3 +7,5 @@ userRouter.get('/:id', (req, res) => {res.send('GET user details')})
 userRouter.post('/', (req, res) => {res.send('CREATE new user')})
 userRouter.put('/:id', (req, res) => {res.send('UPDATE user by id')})
 userRouter.delete('/:id', (req, res) => {res.send('DELETE all users')})  
+
+export default userRouter;
