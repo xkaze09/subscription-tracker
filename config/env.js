49ade5efcdef680cff1c5ba527c8fa7aff9f5985
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-11 02:43:14 -0400

    Update environment configuration to export DB_URI variable

--- config/env.js
+++ config/env.js
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 const env = process.env.NODE_ENV || 'development';
 config({ path: `.env.${env}.local` });
 
-export const { PORT, NODE_ENV } = process.env;
+export const { PORT, NODE_ENV, DB_URI } = process.env;
