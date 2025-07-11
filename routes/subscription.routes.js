58fcb8ef18c84031530fabe8a83f38bee4d89540
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-11 01:34:50 -0400

    Add subscription management routes for CRUD operations and user-specific queries

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -4,4 +4,18 @@ const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => { res.send('GET all subscriptions')})
 
+subscriptionRouter.get('/:id', (req, res) => { res.send('GET subscription details')})
+
+subscriptionRouter.post('/', (req, res) => { res.send('CREATE subscription')})
+
+subscriptionRouter.put('/:id', (req, res) => { res.send('UPDATE subscription')})
+
+subscriptionRouter.delete('/:id', (req, res) => { res.send('DELETE subscription')})
+
+subscriptionRouter.get('/user/:id', (req, res) => { res.send('GET all user subscriptions')})
+
+subscriptionRouter.put('/:id/cancel', (req, res) => { res.send('CANCEL subscription')})
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => { res.send('GET upcoming renewals')})
+
 export default subscriptionRouter;
