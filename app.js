50d49af582bdda6c46ed0a790421585a07d5f103
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-11 02:43:37 -0400

    Integrate MongoDB connection in app startup for database access

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
-
+import connectToDatabase from './database/mongodb.js'
 
 const app = express();
 app.use('/api/v1/auth', authRouter);
@@ -15,8 +15,10 @@ app.get('/', (req, res) => {
     res.send("This is from the server")
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 })
 
 export default app;
