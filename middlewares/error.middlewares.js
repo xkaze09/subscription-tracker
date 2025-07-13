2830de8fbfcf1f9dba08b8639caa1def91140878
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-13 00:15:54 -0400

    Add handling for Mongoose CastError in error middleware to return 404 for resource not found

--- middlewares/error.middlewares.js
+++ middlewares/error.middlewares.js
@@ -5,6 +5,16 @@ const errorMiddleware = (err, req, res, next) => {
         error.message = err.message; 
 
         console.error(err);
+
+        // Mongoose bad ObjectId
+        if (err.name === 'CastError') {
+            const message = 'Resource not found';
+
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+
     } catch (error) {
         next(error);
     }
