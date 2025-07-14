5bdef4a9abd2bf5c8920c0442ab75af3735914bf
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-14 06:46:11 -0400

    Refactor error handling by integrating error middleware into app.js and removing the old error middleware file; enhance application structure and error management.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import { PORT } from './config/env.js';
 
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import connectToDatabase from './database/mongodb.js'
+import errorMiddleware from './middlewares/error.middleware.js'
 
+
+// App
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+// Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+// Middleware
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send("This is from the server")
