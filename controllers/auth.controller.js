5be08242d10cb00676530a9aae869378f31ba12e
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-14 07:49:41 -0400

    Add error handling for session management in sign up; ensure transaction is aborted and session is ended on error to maintain data integrity.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,14 @@ import mongoose from 'mongoose';
 export const signUp = async(req, res, next) => {
     const session = await mongoose.startSession();
     session.startTransaction();
+
+    try {
+
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
 }
 
 export const signIn = async(req, res, next) => {
