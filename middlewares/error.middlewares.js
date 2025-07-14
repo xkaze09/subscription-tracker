6e80336621b49a622459b019f93f4c440890fa02
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-14 00:34:13 -0400

    Add error handling for Mongoose duplicate key and validation errors in error middleware; return appropriate status codes and messages

--- middlewares/error.middlewares.js
+++ middlewares/error.middlewares.js
@@ -9,16 +9,31 @@ const errorMiddleware = (err, req, res, next) => {
         // Mongoose bad ObjectId
         if (err.name === 'CastError') {
             const message = 'Resource not found';
-
             error = new Error(message);
             error.statusCode = 404;
         }
 
+        // Mongoose duplicate key
+        if (err.code === 11000) {
+            const message = 'Duplicate field value entered';
+            error = new Error(message);
+            error.statusCode = 400;
+        }
 
+        // Mongoose validation error
+        if (err.name === 'ValidationError') {
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'});
     } catch (error) {
         next(error);
     }
-}
+};
+
+export default errorMiddleware;
 
 // Create a subscription -> middleware (check for renewal date) -> middleware (check for errors) -> next -> controller
 
