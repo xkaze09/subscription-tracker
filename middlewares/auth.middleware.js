7d1dbb9447b8eedd86233bfcbd33775b8f3bc6c4
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-17 17:41:52 -0400

    Integrate authorization middleware into user routes for enhanced security on user retrieval

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -25,3 +25,5 @@ const authorize = async (req, res, next) => {
         res.status(401).json({message:'Unauthorized', error: error.message});
     }
 }
+
+export default authorize;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
+import authorize from '../middlewares/auth.middleware.js';
+
 import { getUsers, getUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers)
-userRouter.get('/:id', getUser)
+userRouter.get('/:id', authorize, getUser)
 userRouter.post('/', (req, res) => {res.send('CREATE new user')})
 userRouter.put('/:id', (req, res) => {res.send('UPDATE user by id')})
 userRouter.delete('/:id', (req, res) => {res.send('DELETE all users')})  
