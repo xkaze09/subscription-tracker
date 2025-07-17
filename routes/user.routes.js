146ba6aedd17e422a026eaf60d28aeaa4a6ba12d
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-17 17:16:18 -0400

    Refactor user routes to utilize controller functions for retrieving user data

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { getUser, getUsers } from '../controllers/user.controller.js';
+
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {res.send('GET all users')})
-userRouter.get('/:id', (req, res) => {res.send('GET user details')})
+userRouter.get('/', getUsers)
+userRouter.get('/:id', getUser)
 userRouter.post('/', (req, res) => {res.send('CREATE new user')})
 userRouter.put('/:id', (req, res) => {res.send('UPDATE user by id')})
 userRouter.delete('/:id', (req, res) => {res.send('DELETE all users')})  
