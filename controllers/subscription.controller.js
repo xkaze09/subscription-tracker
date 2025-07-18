4c91a04465a91c56989e49feacabb6b114b39f73
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-18 07:34:05 -0400

    Add getUserSubscriptions controller to retrieve user-specific subscriptions with ownership validation

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -12,3 +12,20 @@ export const createSubscription = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        // Check if the user is the same as the one in the token
+        if (req.user.id !== req.params.id) {
+            const error = new Error('You are not the owner of this account');
+            error.status = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user: req.params.id});
+
+        res.status(200).json({success: true, data: subscriptions});
+    } catch (error) {
+        next(error);
+    }
+}
