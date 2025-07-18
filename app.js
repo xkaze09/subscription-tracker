6975830af37eb4908981dedd11f58280307e220c
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-18 06:22:36 -0400

    Add arcjet middleware to app.js for enhanced request processing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
 import connectToDatabase from './database/mongodb.js'
 import errorMiddleware from './middlewares/error.middleware.js'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
 
 
 // App
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 // Routes
 app.use('/api/v1/auth', authRouter);
