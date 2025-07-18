58625e99d70caed09c4c9ae4750170758a7bf64f
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-18 06:46:44 -0400

    Add subscription controller with createSubscription function for handling new subscriptions

---
+++ controllers/subscription.controller.js
@@ -0,0 +1,14 @@
+import Subscription from '../models/subscription.model.js';
+
+export const createSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.create({
+            ...req.body,
+            user: req.user._id,
+        })
+
+        res.status(201).json({success: true, data: subscription});
+    } catch (error) {
+        next(error);
+    }
+}
