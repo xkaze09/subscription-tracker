41cd3ebb8a77e7acec1e90c0d1a225f68b76ced1
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2025-07-18 17:00:50 -0400

    Enhance subscription routes by adding getUserSubscriptions endpoint and integrating authorization middleware for user-specific subscription retrieval

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { createSubscription } from '../controllers/subscription.controller.js';
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
@@ -14,7 +14,7 @@ subscriptionRouter.put('/:id', (req, res) => { res.send('UPDATE subscription')})
 
 subscriptionRouter.delete('/:id', (req, res) => { res.send('DELETE subscription')})
 
-subscriptionRouter.get('/user/:id', (req, res) => { res.send('GET all user subscriptions')})
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions)
 
 subscriptionRouter.put('/:id/cancel', (req, res) => { res.send('CANCEL subscription')})
 
